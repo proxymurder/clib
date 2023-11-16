38069cdf32038212494b725617262c8c2d189c96
proxymurder <30883472+proxymurder@users.noreply.github.com>
2023-11-16 10:47:15 -0600

    decided to keep cxx and c, as an ex. of how little and, at the same time, how much both languages differ... I believe I've done over the past 32h too much thinking on this simple issue, yet there might be some usefull progression in not only the modality of code writte but the logic as well... TODO - README

--- poprand/c/main.c
+++ poprand/c/main.c
@@ -4,21 +4,23 @@
 #include <time.h>
 
 #define N 10
-#define F (10 + N / 10)
-#define FORMAT (i % F != 0 ? "" : "\n")
-#define BREAK (i != n - 1 ? "," : "\n}\n")
-#define T(t) (gettimeofday(&t, NULL))
+#define S (10 + N / 10)
+#define BREAKCOL i % S != 0 ? "" : "\n"
+#define BREAKROW i != n - 1 ? "," : "\n}\n"
+
+#define T 1
+#define TIMEOF(t) gettimeofday(&t, NULL)
+#define TSPEED(i, f, callback) \
+    TIMEOF(i);                 \
+    callback;                  \
+    TIMEOF(f);
 
 float dt_ms(struct timeval i, struct timeval f);
 void dump(int n, int *a, char *tag);
+
 void pop(int *n, int l[], int *p);
 void poprand(int n, int a[]);
 
-// a <array> contains at the end of the algorithm enough information:
-//
-// From left to right you can read the first ones to have popped out
-// and right-most part of <array> a shows the set of remiders or those left out.
-//
 int main()
 {
     srand(time(NULL));
@@ -30,14 +32,15 @@ int main()
     for (int i = 0; i < n; i++)
         a[i] = i;
 
-    T(ti);
+#if T
+    TSPEED(ti, tf, poprand(n, a))
+    printf("done: %f ms\n", dt_ms(ti, tf));
+#else
     poprand(n, a);
-    T(tf);
+#endif
 
     if (n <= 100)
         dump(n, a, "a[n]");
-    printf("done: %f ms\n", dt_ms(ti, tf));
-
     return 0;
 }
 
@@ -46,19 +49,20 @@ float dt_ms(struct timeval i, struct timeval f)
     return ((f.tv_sec - i.tv_sec) * 1000000.0f) + ((f.tv_usec - i.tv_usec) / 1000.0f);
 }
 
+void dump(int n, int *a, char *tag)
+{
+    printf("%s :=> {", tag);
+    for (int i = 0; i < n; i++)
+        printf("%s%i%s", BREAKCOL, a[i], BREAKROW);
+}
+
 void pop(int *n, int l[], int *p)
 {
     int r = rand() % *n;
     int R[*n - r];
-
     *p = l[r];
-
     for (int i = r; i < *n; i++)
-    {
-        R[*n - i] = l[i + 1];
-        l[i] = R[*n - i];
-    }
-
+        l[i] = R[*n - i] = l[i + 1];
     l = realloc(l, --*n * sizeof(int));
 }
 void poprand(int n, int a[])
@@ -68,15 +72,5 @@ void poprand(int n, int a[])
     for (int i = 0; i < n; i++)
         l[i] = a[i];
     while (n > 0)
-    {
-        // Uncomment to view how array modifies over time
-        // dump(n, l, "");
         pop(&n, l, &a[m++]);
-    }
-}
-void dump(int n, int *a, char *tag)
-{
-    printf("%s :=> {", tag);
-    for (int i = 0; i < n; i++)
-        printf("%s%i%s", FORMAT, a[i], BREAK);
 }
---
+++ poprand/cxx/main.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define N 10
+#define S (10 + N / 10)
+#define BREAKCOL i % S != 0 ? "" : "\n"
+#define BREAKROW i != n - 1 ? "," : "\n}\n"
+
+#define T 1
+#define TIMEOF(t) gettimeofday(&t, __null)
+#define TSPEED(i, f, callback) \
+    TIMEOF(i);                 \
+    callback;                  \
+    TIMEOF(f);
+
+float dt_ms(struct timeval i, struct timeval f);
+void dump(int n, int *a, const char *tag = __null);
+
+void pop(int &n, int *(&l), int &p);
+void poprand(int n, int *a);
+
+int main()
+{
+    srand(time(__null));
+    struct timeval ti, tf;
+
+    int n = 10;
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+        a[i] = i;
+
+#if T
+    TSPEED(ti, tf, poprand(n, a))
+    printf("done: %f ms\n", dt_ms(ti, tf));
+#else
+    poprand(n, a);
+#endif
+
+    if (n <= 100)
+        dump(n, a, "a[n]");
+    return 0;
+}
+
+float dt_ms(struct timeval i, struct timeval f)
+{
+    return ((f.tv_sec - i.tv_sec) * 1000000.0f) + ((f.tv_usec - i.tv_usec) / 1000.0f);
+}
+
+void dump(int n, int *a, const char *tag)
+{
+    printf("%s => {", tag);
+    for (int i = 0; i < n; i++)
+        printf("%s%i%s", BREAKCOL, a[i], BREAKROW);
+}
+
+void pop(int &n, int *(&l), int &p)
+{
+    int r = rand() % n;
+    int R[n - r];
+    p = l[r];
+    for (int i = r; i < n; i++)
+        l[i] = R[n - i] = l[i + 1];
+    l = (int *)realloc(l, --n * sizeof(int));
+}
+
+void poprand(int n, int *a)
+{
+    int *l = (int *)calloc(n, sizeof(int));
+    int m = 0;
+    for (int i = 0; i < n; i++)
+        l[i] = a[i];
+    while (n > 0)
+        pop(n, l, a[m++]);
+    free(l);
+}
